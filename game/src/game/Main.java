182c8bb342370262ddc089724294abb89ec99b9b
kwonchanul <kwon_chanul@kangwon.ac.kr>
2016-09-26 22:15:57 +0900

    2016.09.26 10:14

---
+++ game/src/game/Main.java
@@ -0,0 +1,17 @@
+package game;
+
+
+public class Main {
+	public static void main(String args[]){
+		
+		char[][] board = new char[30][17];
+		
+		Frame fm = new Frame();
+
+		fm.printBoard(board);
+	}
+
+	
+	
+}
+
