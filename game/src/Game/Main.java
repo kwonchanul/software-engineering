d60066cad61f4e4858622c2b1132981e43583ea4
kwonchanul <kwon_chanul@kangwon.ac.kr>
2016-09-30 20:59:18 +0900

    2016.09.30 08:57
    Title class 구현
    기본 포지션 및 위치 설정완요

---
+++ game/src/Game/Main.java
@@ -0,0 +1,19 @@
+package Game;
+
+
+public class Main {
+	public static void main(String args[]){
+		
+		new Frame();
+		
+	/*	char[][] board = new char[30][17];
+		
+		Frame fm = new Frame();
+
+		fm.printBoard(board); */
+	}
+
+	
+	
+}
+
