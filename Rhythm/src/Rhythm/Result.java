20e55d34326f9d8f4b081ef60bcee6a482e4cc78
Sm4rtFish <sm4rtf1sh@gmail.com>
2016-11-11 18:26:39 +0900

    Update Result.java

--- Rhythm/src/Rhythm/Result.java
+++ Rhythm/src/Rhythm/Result.java
@@ -1,25 +1,27 @@
 package Rhythm;
 
-import java.awt.event.ActionListener;
+import java.awt.BorderLayout;
+import java.awt.FlowLayout;
 
 import javax.swing.*;
 
 public class Result extends JPanel {
 	private JButton mainmenu, restart;
-	
-
+	JPanel menues;
 	public Result() {
-		//점수 띄우는 내용 삽입 예
+		//점수 띄우는 내용 삽입
 		init();
-	
-		setVisible(true); // 패널이 보임
+		
 	}
 	
 	void init(){
+		menues = new JPanel();
+		this.setLayout(new BorderLayout());
 		mainmenu = new JButton("메인 메뉴"); // 메인메뉴 버튼
 		restart = new JButton("다시 시작"); // 다시 시작 버튼
-		this.add(mainmenu);
-		this.add(restart);
+		menues.add(mainmenu);
+		menues.add(restart);
+		this.add(menues, BorderLayout.SOUTH);
 	}
 }
 	public void restart(){
