8877f8728507ae9a9704629d0933a7e723edd779
Sm4rtFish <sm4rtf1sh@gmail.com>
2016-11-20 14:00:51 +0900

    Update Result.java

--- Rhythm/src/Rhythm/Result.java
+++ Rhythm/src/Rhythm/Result.java
@@ -1,17 +1,30 @@
 package Rhythm;
+
 /*************
 * author : NGH
 **************/
 import java.awt.BorderLayout;
-import java.awt.FlowLayout;
+import java.awt.Font;
+import java.awt.Label;
 
-import javax.swing.*;
+import javax.swing.JButton;
+import javax.swing.JPanel;
 
 public class Result extends JPanel {
-	private JButton mainmenu, restart;
-	JPanel menues;
+	private JButton mainmenu, restart; // 버튼
+	private String s = "10";  // 점수. 실험용임
+	private Font bold; //글씨체 변수
+	JPanel menues; //메뉴 패널. 밑에 깔려있음
+	
 	public Result() {
-		//점수 띄우는 내용 삽입
+		//점수 띄우는 내용
+		Label score = new Label();
+		score.setText(s);
+		score.setAlignment(score.CENTER);
+		score.setSize(400, 400);
+		bold = new Font("궁서", Font.BOLD, 100);
+		this.add(score);
+		this.setFont(bold);
 		init();
 		
 	}
@@ -26,7 +39,7 @@ public class Result extends JPanel {
 		this.add(menues, BorderLayout.SOUTH);
 	}
 }
-	public void restart(){
+	/*public void restart(){
 		handle.backTitle();
 	}
-}
+}*/
